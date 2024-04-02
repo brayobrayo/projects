4c83aca39a947d27b85ac112b6f40fd1f2f93c33
BRIAN400400 <162319476+BRIAN400400@users.noreply.github.com>
2024-04-02 13:47:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
   
-    
-      function welcomeUser() {
-          var name = document.getElementById("name").value;
-          var surname = document.getElementById("surname").value;
-          var phone = document.getElementById("phone").value;
-          var welcomeMessage = "Welcome " + name + " "+surname+" your phone number is "+phone+"!";
-          document.getElementById("welcomeMessage").textContent = welcomeMessage;
-      }
-      
+    function welcomeUser() {
+      var traintype = document.getElementById("traintype").value;
+      var origin = document.getElementById("origin").value;
+      var destination = document.getElementById("destination").value;
+      var date = document.getElementById("date").value;
+      var welcomeMessage = "Our dear customer, note that you selected " + traintype + " train departing from " + origin + " traveling to " + destination + " and your date of travel is " + date + ". Thank you for engaging with us.";
+      document.getElementById("welcomeMessage").textContent = welcomeMessage;
+    }
