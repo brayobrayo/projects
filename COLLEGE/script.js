0c18437e3f85ae6a993c07a6eefebd4b4066f259
BRIAN MUTISO <162319476+brayobrayo@users.noreply.github.com>
2025-03-11 14:01:06 +0300

    Update script.js

--- COLLEGE/script.js
+++ COLLEGE/script.js
@@ -5,3 +5,19 @@ function showSection(sectionId) {
             });
             document.getElementById(sectionId).classList.add('active');
 }
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "PrintScreen") {
+        event.preventDefault();
+        alert("Screenshots are disabled on this site!");
+    }
+    if (event.ctrlKey && event.key === "p") {  // Prevent print
+        event.preventDefault();
+        alert("Printing is disabled!");
+    }
+});
+
+document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+    alert("Right-click is disabled!");
+});
