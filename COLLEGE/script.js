fe494aa555de262f267792c5a51d38d497a3f8b1
BRIAN MUTISO <162319476+brayobrayo@users.noreply.github.com>
2025-06-23 13:11:47 +0300

    Update script.js

--- COLLEGE/script.js
+++ COLLEGE/script.js
@@ -1,23 +1,32 @@
+//display section based on the button clicked in navigation bar
 function showSection(sectionId) {
-            const sections = document.querySelectorAll('section');
-            sections.forEach(section => {
-                section.classList.remove('active');
-            });
-            document.getElementById(sectionId).classList.add('active');
-}
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => section.classList.remove('active'));
+    document.getElementById(sectionId).classList.add('active');
+  }
 
-document.addEventListener("keydown", function (event) {
-    if (event.key === "PrintScreen") {
-        event.preventDefault();
-        alert("Screenshots are disabled on this site!");
-    }
-    if (event.ctrlKey && event.key === "p") {  // Prevent print
-        event.preventDefault();
-        alert("Printing is disabled!");
-    }
-});
+//display submenu from an item in main menu  
+function toggleSubmenu(item) {
+    item.classList.toggle("expanded");
+  }
+//display sidebar menu from three bar button 
+  function toggleSidebar(icon) {
+    icon.classList.toggle("active");
+    const sidebar = document.getElementById("mySidebar");
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+      } else {
+        sidebar.style.width = "250px";}
+  
 
-document.addEventListener("contextmenu", function (event) {
+  // Disable screenshot/print
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "PrintScreen" || (event.ctrlKey && event.key === "p")) {
+      event.preventDefault();
+      alert("Screenshots and printing are disabled!");
+    }
+  });
+  document.addEventListener("contextmenu", function (event) {
     event.preventDefault();
     alert("Right-click is disabled!");
-});
+  });}
